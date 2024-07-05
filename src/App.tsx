7056a0e29178a9d9ec5644f7311eed9eeb88c74c
Sangeetha <sangeetha@RRs-MacBook-Air.local>
2024-07-05 05:02:54 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import GlobalStyle from './GlobalStyles';
+
+function App() {
+  return (
+    <>
+    <GlobalStyle/>
+    <div className="App">
+     <h1>Welcome to property portal</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
